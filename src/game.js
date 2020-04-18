2e9ff27672a41fdef0ceb46e50282b5cb9f6dfe3
Magnus Conrad Hyll <magnus@hyll.no>
2020-04-18 15:40:31 +0200

    Initial code

---
+++ src/game.js
@@ -0,0 +1,28 @@
+import * as THREE from "three";
+let scene = new THREE.Scene();
+let camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+let renderer = new THREE.WebGLRenderer();
+renderer.setSize(window.innerWidth, window.innerHeight);
+document.body.onresize = (ev) => {
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    camera.aspect = window.innerWidth / window.innerHeight;
+    camera.updateProjectionMatrix();
+};
+document.body.appendChild(renderer.domElement);
+let geometry = new THREE.BoxGeometry();
+let material = new THREE.MeshPhongMaterial({ color: 0x00ff00 });
+let cube = new THREE.Mesh(geometry, material);
+scene.add(cube);
+const color = 0xFFFFFF;
+const intensity = 1;
+const light = new THREE.DirectionalLight(color, intensity);
+light.position.set(-1, 2, 4);
+scene.add(light);
+camera.position.z = 5;
+function render() {
+    window.requestAnimationFrame(render);
+    cube.rotation.x += 0.01;
+    cube.rotation.y += 0.01;
+    renderer.render(scene, camera);
+}
+render();
